115bf95fe3f8f1b680ae5c5d11387c6e522bdcf7
TaRCioS-CLaY <tarcios.clay@gmail.com>
2019-11-22 00:28:51 -0300

    Variaveis de ambiente

---
+++ src/services/beneficiarios.service.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { trocarValorKey } from './utils.service';
+const enderecoApi = process.env.API || 'http://teamb2019-com.umbler.net';
+/**
+ * Pega todos os beneficiários da base
+ * @returns {Promise} Uma Promise com um array de beneficiários
+ */
+export function pegarBeneficiarios(){
+    return axios.get(enderecoApi + '/beneficiarios').then((dados) => {
+      const array = dados.data.map((e) => {
+        e = trocarValorKey('nr_matricula', 'value', e);
+        e = trocarValorKey('nm_beneficiario', 'text', e);
+        return e;
+      });
+      return array;
+    });
+  }
---
+++ src/services/demonstrativos.service.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import { trocarValorKey } from './utils.service';
+const enderecoApi = process.env.API || 'http://teamb2019-com.umbler.net';
+/**
+ * Pega todas as despesas da base
+ * @returns {Promise} Uma Promise com um array de despesas
+ */
+export function pegarTodasDespesas(){
+    return axios.get(enderecoApi + '/despesas').then((dados) => {
+      const array = dados.data.map((e) => {
+        e = trocarValorKey('dt_atendimento', 'data', e);
+        e = trocarValorKey('ds_credenciado', 'beneficiario', e);
+        e = trocarValorKey('ds_procedimento', 'consulta', e);
+        e = trocarValorKey('vl_procedimento', 'valor', e);
+        return e;
+      });
+      return array;
+    });
+  }
+
+/**
+ * Pega as despesas do beneficiário dono do Id passado
+ * @param {string} id Id do beneficiário
+ * @returns {Promise} Uma Promise com um array de despesas
+ */
+export function pegarDespesasPorId(id){
+    return axios.get(enderecoApi + '/despesas', {params: {id: id}}).then((dados) => {
+      const array = dados.data.map((e) => {
+        e = trocarValorKey('dt_atendimento', 'data', e);
+        e = trocarValorKey('ds_credenciado', 'credenciado', e);
+        e = trocarValorKey('ds_procedimento', 'consulta', e);
+        e = trocarValorKey('vl_procedimento', 'valor', e);
+        return e;
+      });
+      return array;
+    });
+  }
